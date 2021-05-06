b7e588f56580d0bd29d35e416e9d966f119c3336
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-05-07 00:31:16 +0530

    output done

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+
+
+
+console.log(outputDiv);
 
 function clickHandler() {
     console.log("clicked!");
