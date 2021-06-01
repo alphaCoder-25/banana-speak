362286e7572e85ba3266090d498fb48026b6f6dc
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-06-02 01:07:11 +0530

    changes

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
-var txtInput = document.querySelector("#txt-input");
-
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function getTranslationURL(text) { 
+     return serverURL + "?" + "text=" + text
+}
 
 function clickHandler() {
-    outputDiv.innerText = "hjjs hjjs hjjs " + txtInput.value;
+
+    // outputDiv.innerText = "ajjjsjkjjaajs " +  txtInput.value;
+
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json))
 };
 
 btnTranslate.addEventListener("click", clickHandler)
